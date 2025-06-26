926ddde56b86041867d46f313b4afba2ed631292
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-06-26 09:37:28 +0900

    初期設定

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+require_once "header.php";
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>TaskBeat</title>
+</head>
+<body>
+    <header>
+        <h3 class="">メニュー</h3>
+        <nav class="header__nav nav" id="js-nav">
+            <ul class="nav__items nav-items">
+                <li class="nav-items__item"><a href="">メニュー</a></li>
+                <li class="nav-items__item"><a href="">メニュー</a></li>
+                <li class="nav-items__item"><a href="">メニュー</a></li>
+                <li class="nav-items__item"><a href="">メニュー</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
