7cf1418326f3220acbed33039a6ef47f76a154a2
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-06-27 13:33:04 +0900

    fontcolor交換

--- index.php
+++ index.php
@@ -15,12 +15,26 @@
         <h3 class="">メニュー</h3>
         <nav class="header__nav nav" id="js-nav">
             <ul class="nav__items nav-items">
-                <li class="nav_item"><a href="">ホーム</a></li>
-                <img src="img/home.png" alt="">
-                <li class="nav_item"><a href="">BGM大辞典</a></li>
-                <li class="nav_item"><a href="">フレンドリスト</a></li>
-                <li class="nav_item"><a href="">プロフィール</a></li>
-                <li class="nav_item"><a href="">カレンダー</a></li>
+                <div class="headeritem">
+                    <img src="img/home.png" alt="">
+                    <li class="nav_item">ホーム</a></li>
+                </div>
+                <div class="headeritem">
+                    <img src="img/book.png" alt="">
+                    <li class="nav_item"><a href="">BGM大辞典</a></li>
+                </div>
+                <div class="headeritem">
+                    <img src="img/friend.png" alt="">
+                    <li class="nav_item"><a href="">フレンドリスト</a></li>
+                </div>
+                <div class="headeritem">
+                    <img src="img/people.png" alt="">
+                    <li class="nav_item"><a href="">プロフィール</a></li>
+                </div>
+                <div class="headeritem">
+                    <img src="img/calendar.png" alt="">
+                    <li class="nav_item"><a href="">カレンダー</a></li>
+                </div>
             </ul>
         </nav>
     </header>
